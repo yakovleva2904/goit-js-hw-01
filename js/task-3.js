2a40fa0313099dee18bf82db4daf63b61a88aa84
yakovleva2904 <yakovlevaolga2904@gmail.com>
2024-06-11 22:12:15 +0200

    Update task-3.js
    third task is done

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    const totalWidth = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+    return totalWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
